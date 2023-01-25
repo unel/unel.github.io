e9d206278c0c58a4caa3586e100f95caf9f7b76f
Peter Jah <unel@bk.ru>
2023-01-25 21:25:00 +0300

    add some events to test sw

--- root-sw.js
+++ root-sw.js
@@ -18,9 +18,33 @@ function onFetch(event) {
 	notify('event fired :: fetch', event);
 }
 
+function onMessage(event) {
+	notify('event fired :: message', event);
+}
+
+function onPortMessage(event) {
+	notify('event fired :: port message', event);
+}
+
+function onConnect(event) {
+	notify('event fired:: connect', event);
+
+	for (const port of event.ports) {
+		notify('listening port..', port);
+		port.addEventListener('message', onPortMessage);
+	}
+}
+
+function onSync(event) {
+	notify('event fired :: sync event', event);
+}
+
 // subsctibers
 self.addEventListener('activate', onActivate);
 self.addEventListener('install', onInstall);
 self.addEventListener('fetch', onFetch);
+self.addEventListener('connect', onConnect);
+self.addEventListener('message', onMessage);
+self.addEventListener('sync', onSync);
 
 notify('evaluated', { self });
