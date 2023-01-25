e8161353aaee0a45ce658e13c95f8bec3a0b8290
Peter Jah <unel@bk.ru>
2023-01-25 20:12:01 +0300

    introduce test service worker

---
+++ js/modules/register-sw.js
@@ -0,0 +1,27 @@
+export function registerSW(params) {
+	return new Promise((resolve, reject) => {
+		try {
+			const { url, notify = ()=>{} } = params;
+			const registration = await navigator.serviceWorker.register(url, {
+				scope: '/'
+			});
+
+			switch (true) {
+				case registration.installing:
+					notify('installing');
+					break;
+
+				case registration.waiting:
+					notify('waiting');
+					break;
+
+				case registration.active:
+					notify('active');
+					resolve();
+					break;
+			}
+		} catch (e) {
+			reject(e);
+		}
+	});
+}
---
+++ js/service-workers/modules-provider.js
@@ -0,0 +1,17 @@
+const swName = 'sw/modules-provider';
+
+function notify(message, ...args) {
+	console.log(`${swName}:: ${message}`, ...args);
+}
+
+self.addEventListener('activate', (event) => {
+	notify('event fired :: activate', event);
+});
+
+self.addEventListener('install', (event) => {
+	notify('event fired :: install', event);
+});
+
+self.addEventListener('fetch', (event) => {
+	notify('event fired :: fetch', event);
+});
